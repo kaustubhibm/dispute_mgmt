7c4fa05e3250722e216100aabb86e6016ab886f6
Kaustubh Oak <kaustubh@in.ibm.com>
2016-12-11 22:53:19 +0530

    augmented code

--- dispute_mgmt.go
+++ dispute_mgmt.go
@@ -63,6 +63,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 			jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 			return nil, errors.New(jsonResp)
 		}
+		jsonResp = "{\"" + valAsbytes + "\"}"
+		
 		return valAsbytes, nil
 	}
 	return nil, nil
